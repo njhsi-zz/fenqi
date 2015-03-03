be9e0c389b5794a74c6420dc4f83a90c68e83bf1
Nj Xiong <nj.hsiong@gmail.com>
2015-03-03 11:18:09 +0800

    draft

--- fenqi.py
+++ fenqi.py
@@ -3,3 +3,44 @@
 '''
 Fenqi aka amortization calculator for loan and credicards.
 '''
+
+def amortizationSchedule( principal, term, rate ):
+   pmt = ( principal * rate * ( 1 + rate)**term ) / (( 1 + rate)**term - 1)
+   pmt = round(pmt,2) # people rarely pay in fractional pennies
+   remainingPrincipal = principal
+   for pd in range(1,term+1):
+      if pd < term:
+          pdInterest = rate * remainingPrincipal
+          pdInterest = round(pdInterest,2)
+          pdPmt = pmt
+      else:
+          pdInterest = 0
+          pdPmt = remainingPrincipal
+      pdPrincipal = pdPmt - pdInterest
+      remainingPrincipal -= pdPrincipal
+      yield pd, pdPmt, pdInterest, pdPrincipal, remainingPrincipal
+ 
+ 
+ 
+ 
+ 
+##################################
+import datetime
+
+P,Y,R,I,detail = 360000,15,3.87,6000,1
+## (Date, Principal, Months, Interest rate)
+plans = [
+    (datetime.date(2009,04,01), 36000, 15*12, 3.87/12/100),
+    ]
+
+## <date: (pay, principal part of pay, interest part of pay, remaining principal)>
+terms = {}
+D,P,N,I =0,0,0,0
+for (pD, pP, pN, pI) in plans:
+    D, P, N, I =D, P+pD, N+pN, pI
+    for(i, pmt, int, princ, remaining) amortizationSchedule(P, N, I):
+      terms[D+i] = (pmt, princ, int, remaining)
+    #todo: remove terms after D
+
+    
+print terms
